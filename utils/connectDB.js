ae6d617eb315cc79ba518e91de606b61fff1baa7
andybro <132149967+mrlit-a@users.noreply.github.com>
2025-01-05 14:55:30 -0500

    Add files via upload

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const mongoDB = "mongodb+srv://maxiiiii:yooo6916@cluster0.qsehi0c.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = "mongodb+srv://Ethix:aTcWrkehiy5xvfDA@ethix.6cua0.mongodb.net/?retryWrites=true&w=majority&appName=Ethix";
 
 let isConnected = false;
 
