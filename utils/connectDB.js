96ca2713f420594e7a7a62f606a6fb839a2939e0
kenshinaru <121244764+kenshinaru@users.noreply.github.com>
2025-01-06 11:23:44 +0700

    Update connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const mongoDB = "mongodb+srv://Ethix:aTcWrkehiy5xvfDA@ethix.6cua0.mongodb.net/?retryWrites=true&w=majority&appName=Ethix";
+const mongoDB = "mongodb+srv://sweet:sweet@cluster0.h9lfoxc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 let isConnected = false;
 
