140d0974e459a69d8813753458072cf9ce954f4b
kenshinaru <121244764+kenshinaru@users.noreply.github.com>
2025-01-06 23:30:24 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,9 +14,11 @@ import path from 'path';
 import 'node-fetch';
 import 'axios';
 import 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { doReact, emojis } from '../lib/autoreact.cjs';
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 const app = express();
 const PORT = process.env.PORT || 3000;
