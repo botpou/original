e5a9eb4e81fc7d69c73ab044328b395cd24dac43
kenshinaru <121244764+kenshinaru@users.noreply.github.com>
2025-01-06 12:21:02 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -283,7 +283,6 @@ async function createBot(sessionId) {
         const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
         if (shouldReconnect) {
           console.log("Connection lost, attempting to reconnect...");
-          await deleteSession(sessionId);
           setTimeout(() => createBot(sessionId), 5000);
         } else {
           console.log(sessionId + " Logged out.");
