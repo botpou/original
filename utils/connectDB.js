aa8ff45db3a9b0dee005c0860ec8ef4e00187477
shizandy <shizxyandy+@gmail.com>
2025-05-23 19:38:17 -0500

    Add files via upload

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -10,14 +10,32 @@ const ConnectDB = async () => {
   }
 
   try {
-    await mongoose.connect(mongoDB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    // Remove deprecated options
+    await mongoose.connect(mongoDB);
+    
     isConnected = true;
     console.log("Connected to MongoDB.");
+    
+    // Handle connection events
+    mongoose.connection.on('error', (err) => {
+      console.error('MongoDB connection error:', err);
+      isConnected = false;
+    });
+    
+    mongoose.connection.on('disconnected', () => {
+      console.log('MongoDB disconnected');
+      isConnected = false;
+    });
+    
+    mongoose.connection.on('reconnected', () => {
+      console.log('MongoDB reconnected');
+      isConnected = true;
+    });
+    
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
+    isConnected = false;
+    throw error;
   }
 };
 
